c1f513c54095869ba4cc3f5611d5efebd347ee10
Frolov Kirill <frolovdata0238@gmail.com>
2023-08-01 18:37:32 +0700

    Fixing bash

--- run_api.sh
+++ run_api.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git clone https://github.com/jokkojja/invian_testovoe.git
-cd invian_testovoe
 python3 -m venv testovoe.env
 source testovoe.env/bin/activate
 pip install -r requirements.txt
